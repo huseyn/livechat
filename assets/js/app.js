4e80e95ba2657280d8871d8a554e2cf7ba9ce40f
Huseyn <huseyn.mikayil95@gmail.com>
2020-02-05 11:11:06 +0400

    chat functionality was resolved

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+const startedChatBtn = document.getElementById('starterBox');
+const chatBox = document.getElementById('chatBox');
+const chatInput = document.getElementById('chatInput');
+
+const startChat = () => {
+    startedChatBtn.classList.add('nonvisible');
+    chatBox.classList.remove('nonvisible');
+}
+
+const getInput = () => {
+    if (chatInput.value === '') {
+        return;
+    } else {
+        return chatInput.value;
+    }
+}
+
+const addMessageHandler = () => {
+    const message = getInput();
+    let image = '';
+    let innerText = '';
+    const li = document.createElement('li');
+    const ul = document.getElementById('chatList');
+
+    if(message.match(new RegExp(/^[A-Z]/)) !== null){
+        li.className = 'left';
+        image = 'operator';
+        innerText =` 
+        <img src="assets/images/${image}.jpg" alt="">
+        <p>${message}</p>
+        `;
+    }else{
+        li.className = 'right';
+        image = 'user';
+        innerText =` 
+        <p>${message}</p>
+        <img src="assets/images/${image}.jpg" alt="">
+        `;
+    }
+
+
+    li.innerHTML =innerText;
+
+    ul.append(li);
+}
+
+chatInput.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
+        addMessageHandler();
+    const chatList = document.getElementById('chatList');    
+    chatList.scrollTop = chatList.scrollHeight;
+
+    }
+});
